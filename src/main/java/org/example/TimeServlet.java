16f38a6657feda761c6bafa20938272c928c3691
gtsasiuk <gtsasuk@gmail.com>
2024-11-11 21:03:36 +0200

    Updated to version DevHomework11

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -2,9 +2,13 @@ package org.example;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+import org.thymeleaf.TemplateEngine;
 
 import java.io.IOException;
 import java.time.ZoneId;
@@ -14,6 +18,18 @@ import java.time.format.DateTimeFormatter;
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    private TemplateEngine engine;
+
+    @Override
+    public void init() throws ServletException {
+        engine = new TemplateEngine();
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix(getServletContext().getRealPath("/WEB-INF/templates/"));
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -32,9 +48,15 @@ public class TimeServlet extends HttpServlet {
         ZonedDateTime zonedDateTime = ZonedDateTime.now(zoneId);
         String formattedTime = zonedDateTime.format(formatter);
 
-        req.setAttribute("currentTime", formattedTime);
-        req.setAttribute("timezone", zoneId.toString());
+        Cookie timezoneCookie = new Cookie("lastTimezone", timezone);
+        timezoneCookie.setPath("/");
+        resp.addCookie(timezoneCookie);
 
-        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        Context context = new Context();
+        context.setVariable("currentTime", formattedTime);
+        context.setVariable("timezone", zoneId.toString());
+
+        engine.process("time", context, resp.getWriter());
+        resp.getWriter().close();
     }
 }
